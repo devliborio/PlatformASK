3a43b857ec89e69135fbc5fc1b89d20f1c96c9b3
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-02 14:26:25 -0300

    Organizando espaçamentos

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express"); // Importando ExpressJS
 const app = express(); // Criando uma instância do ExpressJS
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
     res.render("index");
-})
+});
 
-app.listen(2108,(erro) =>{
-    if(erro){
+app.listen(2108, (erro) => {
+    if (erro) {
         console.log("O servidor está com erro!")
     } else {
         console.log("O servidor está rodando normalmente!")
