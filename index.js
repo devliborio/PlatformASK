b2782c2ca4461f8d058dfe3d6846ffae6b2dd02a
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-03 11:09:31 -0300

    Importando conexão do sequelize com o BD e autenticando a mesma

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express"); // Importando ExpressJS
 const app = express(); // Criando uma instância do ExpressJS
 const bodyParser = require("body-parser"); // Importando body-parser
+const conection = require("./database/conection");
+
+conection.authenticate().then(() => {
+    console.log("Conexão feita com o banco de dados!");
+}).catch((msgErro) => {
+    console.log(msgErro);
+});
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({extended: false})); // Decodificando os dados enviados pelo formulario para serem manipulados pelo JS
+app.use(bodyParser.urlencoded({ extended: false })); // Decodificando os dados enviados pelo formulario para serem manipulados pelo JS
 app.use(bodyParser.json()); // Permite com que a gente consiga ler dados de formularios enviados por .json
 
 app.get("/", (req, res) => {
