f344131c12f534c3eb57be8aed7c68ebfab3c08a
Guilherme Libório Machado <liborio.ofc@gmail.com>
2023-02-03 12:28:48 -0300

    Importando model de perguntas no index.js

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require("express"); // Importando ExpressJS
 const app = express(); // Criando uma instância do ExpressJS
 const bodyParser = require("body-parser"); // Importando body-parser
-const conection = require("./database/conection");
+const conection = require("./database/conection"); // Importando conexão do Sequelize com Banco de dados
+const askModel = require("./database/Ask"); // Importando model de perguntas
 
+// Autenticação da conexão do Sequelize com o Banco de dados
 conection.authenticate().then(() => {
     console.log("Conexão feita com o banco de dados!");
 }).catch((msgErro) => {
     console.log(msgErro);
 });
 
+// ExpressJS
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+// Body-Parser
 app.use(bodyParser.urlencoded({ extended: false })); // Decodificando os dados enviados pelo formulario para serem manipulados pelo JS
 app.use(bodyParser.json()); // Permite com que a gente consiga ler dados de formularios enviados por .json
 
+// Rotas
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -28,6 +33,7 @@ app.post("/saveask", (req, res) => {
     var description = req.body.description
 });
 
+// Servidor
 app.listen(2108, (erro) => {
     if (erro) {
         console.log("O servidor está com erro!")
